cd52a47ee0605c9cb2a24c70fdd76893462fd68f
Vinh Le <tun27687@temple.edu>
2022-09-13 10:04:42 -0400

    assigment1- second commit

--- app/src/main/java/com/example/assignemt1/MainActivity.kt
+++ app/src/main/java/com/example/assignemt1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.assignemt1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textview)
+        findViewById<View>(R.id.button).setOnClickListener{textView.text = "Clicked!"}
     }
 }
